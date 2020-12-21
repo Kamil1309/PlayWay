1c09af48cfb72eeddebc3962c27438facf36e7b8
STUDIES <kamil.szostek97@gmail.com>
2020-12-21 06:47:52 +0100

    poprawki

--- Projekt03 - podnoszenie przedmiotow/Assets/Scripts/Items/Item.cs
+++ Projekt03 - podnoszenie przedmiotow/Assets/Scripts/Items/Item.cs
@@ -11,19 +11,25 @@ public class Item : MonoBehaviour, IInteractable
 
     public Texture2D inventorySprite;
 
+    float angleMultiplier = 2 * Mathf.PI/360.0f;
+    float angleShift = Mathf.PI * 1/2.0f;
+    float acceptanceAngle = 30/360.0f * Mathf.PI;
+
     public virtual void OnUse(){}
 
     public bool CanInteract(GameObject player)
     {   
         if(gameObject.activeSelf)
         {
-            float playerDirRad = player.transform.localEulerAngles.y/360.0f * 2 * Mathf.PI;
-            playerDirRad = -playerDirRad + Mathf.PI * 1/2.0f;
+            float playerDirRad = player.transform.localEulerAngles.y * angleMultiplier;
+            playerDirRad = -playerDirRad + angleShift;
 
-            Vector2 itemPlayer = new Vector2(gameObject.transform.position.x - player.gameObject.transform.position.x, gameObject.transform.position.z - player.gameObject.transform.position.z);
+            Vector2 itemPlayer = new Vector2(gameObject.transform.position.x - player.gameObject.transform.position.x, 
+                                            gameObject.transform.position.z - player.gameObject.transform.position.z);
             Vector2 playerDirection = new Vector2(Mathf.Cos(playerDirRad), Mathf.Sin(playerDirRad));
 
-            if( Vector2.Dot(itemPlayer, playerDirection)/(itemPlayer.magnitude * playerDirection.magnitude) > Mathf.Cos(30/360.0f * Mathf.PI)){
+            if( Vector2.Dot(itemPlayer, playerDirection)/(itemPlayer.magnitude * playerDirection.magnitude) > 
+                                        Mathf.Cos(acceptanceAngle)){
                 return true;
             }
         }
@@ -31,18 +37,28 @@ public class Item : MonoBehaviour, IInteractable
     }
 
     public void Interact(GameObject player){ 
-        player.GetComponent<InventorySystem>().Items.Add(gameObject);
-        player.GetComponent<InventorySystem>().currentCapacity += _weight;
-        gameObject.SetActive(false);
+        if(player.GetComponent<InventorySystem>() != null){
+            player.GetComponent<InventorySystem>().Items.Add(gameObject);
+            player.GetComponent<InventorySystem>().currentCapacity += _weight;
+            gameObject.SetActive(false);
+        }
+        else{
+            Debug.Debug.LogError("No component 'InventorySystem'!!!");
+        }
     }
 
     public bool CheckCapacityLimit(GameObject player){
-        if(player.GetComponent<InventorySystem>().currentCapacity + _weight <= player.GetComponent<InventorySystem>().maxCapacity){
-            return true;
+        if(player.GetComponent<InventorySystem>() != null){
+            if(player.GetComponent<InventorySystem>().currentCapacity + _weight <= player.GetComponent<InventorySystem>().maxCapacity){
+                return true;
+            }
+            else{
+                Debug.Log("Weight too big!!! You can't pick it up :(");
+                return false;   
+            }
         }
         else{
-            Debug.Log("Weight too big!!! You can't pick it up :(");
-            return false;   
+            Debug.Debug.LogError("No component 'InventorySystem'!!!");
         }
     }
 }
